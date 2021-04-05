69006de84f61843c5f03289ceb27cc9d3811d376
fariha <fariha_ahmed27@hotmail.com>
2021-04-05 20:52:56 +0500

    List Demo

---
+++ listDemo.py
@@ -0,0 +1,52 @@
+mylist=["Aqib","Atif","Aneeq","Imad","Aliyan","Aliyan"]
+print(mylist)
+print(len(mylist))
+print(type(mylist))
+print(mylist[2])
+
+# change value
+mylist[3]="jaffar"
+print(mylist)
+#change value in a range
+mylist[1:3]=["abc","xyz"]
+print(mylist)
+
+#remove from list
+mylist.remove("Aliyan")
+print(mylist)
+
+# remove through index
+mylist.pop(3)
+print(mylist)
+
+# remove last element5
+mylist.pop()
+print(mylist)
+
+#del from list
+del mylist[0]
+print(mylist)
+
+#clear from all 
+#mylist.clear()
+#print(mylist)
+
+#append /insert 
+mylist.append("Imad")
+print(mylist)
+
+#insert through index
+mylist.insert(1,"intekhab")
+print(mylist)
+
+lista=["a","e","i","o","u"]
+listb=["A","E","I","O","U"]
+
+#append list
+lista.extend(listb)
+print(lista)
+
+
+
+
+
